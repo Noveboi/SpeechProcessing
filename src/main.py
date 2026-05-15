176aa2598d88fb368706cb1c0730cb971f37ac3d
George Nikolaidis <nove@novesoftware.net>
2026-05-15 17:49:25 +0300

    Add audio processor module

--- src/main.py
+++ src/main.py
@@ -1,2 +1,9 @@
+from scipy.io import wavfile
+
+from process import AudioProcessor
+
 if __name__ == "__main__":
-    pass
+    fs, audio = wavfile.read("test.wav")
+    processor = AudioProcessor(audio, fs)
+
+    processor._print()
