39fd15fd8e58c2f5ae63bbfba92d5df6b70298e3
George Nikolaidis <nove@novesoftware.net>
2026-05-15 18:31:57 +0300

    Remove process.py, introduce after the pre-processing modules are complete

--- src/main.py
+++ src/main.py
@@ -1,9 +1,2 @@
-from scipy.io import wavfile
-
-from process import AudioProcessor
-
 if __name__ == "__main__":
-    fs, audio = wavfile.read("test.wav")
-    processor = AudioProcessor(audio, fs)
-
-    processor._print()
+    pass
