0fcb97c48ff7155d0996f7c77b06df8b2a85bd78
George Nikolaidis <nove@novesoftware.net>
2026-05-16 09:13:41 +0300

    Update main.py to test out pre-emphasis

--- src/main.py
+++ src/main.py
@@ -1,2 +1,12 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.io import wavfile
+
+import loader
+import processor
+
 if __name__ == "__main__":
-    pass
+    audio = loader.load_audio("test.wav")
+    emphasized = processor.pre_emphasis(audio)
+
+    wavfile.write("em.wav", emphasized.sample_rate, emphasized.waveform)
