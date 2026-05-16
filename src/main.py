4583ad63cefa7c96f18ca11b58c28667f85f160a
George Nikolaidis <nove@novesoftware.net>
2026-05-16 11:06:13 +0300

    Update main.py to include visualization after feature extraction

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ import numpy as np
 from scipy.io import wavfile
 
 import loader
+import plots
 import processor
 
 
@@ -16,6 +17,18 @@ def main():
     )
 
     audio = loader.load_audio("test.wav")
-    emphasized = processor.pre_emphasis(audio)
+    audio_emphasis = processor.pre_emphasis(audio)
+    frames = processor.split_into_frames(audio_emphasis, 25, 10, "hamming")
+    features = processor.extract(frames)
+    mfcc_matrix = features[:, 2:41]
 
-    wavfile.write("em.wav", emphasized.sample_rate, emphasized.waveform)
+    plots.plot_waveform(audio, audio_emphasis)
+    plots.plot_power_spectrum(frames[50])
+    plots.plot_mel_filterbank(frames[0])
+    plots.plot_spectrogram(frames)
+    plots.plot_mfccs(mfcc_matrix)
+    plots.plot_time_domain_features(features)
+
+
+if __name__ == "__main__":
+    main()
