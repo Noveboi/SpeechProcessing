831761a972689809154920d4f20c04f0df4c6f1d
George Nikolaidis <nove@novesoftware.net>
2026-05-22 22:58:08 +0300

    Implement read_csv method and use better error handling

--- src/files.py
+++ src/files.py
@@ -2,19 +2,45 @@ import csv
 import logging
 from pathlib import Path
 
-from common import Segment
+from common import Segment, SegmentLabel
 
 log = logging.getLogger(__name__)
 
+CSV_FIELDNAMES = ["Audiofile", "start", "end", "class"]
 
-def read_csv(file_path: str): ...
+
+def read_csv_as_segments(file_path: str) -> list[Segment] | None:
+    """
+    Read a CSV and translate it into background/foreground segments
+    """
+
+    segments: list[Segment] = []
+
+    try:
+        with open(file_path, "r", newline="") as f:
+            f.readline()  # skip headers
+            reader = csv.DictReader(f, fieldnames=CSV_FIELDNAMES)
+
+            for row in reader:
+                segments.append(
+                    Segment(
+                        start=float(row["start"]),
+                        end=float(row["end"]),
+                        label=SegmentLabel(row["class"]),
+                    )
+                )
+    except OSError as err:
+        log.critical("Couldn't read CSV file: %s", err.strerror, exc_info=err)
+        return None
+
+    return segments
 
 
 def write_csv(
     segments: list[Segment],
     audio_filename: str,
     output_path: str,
-) -> None:
+) -> bool:
     """
     Write the segment list to a CSV file in the required format:
 
@@ -31,17 +57,22 @@ def write_csv(
     """
     Path(output_path).parent.mkdir(parents=True, exist_ok=True)
 
-    with open(output_path, "w", newline="") as f:
-        writer = csv.DictWriter(f, fieldnames=["Audiofile", "start", "end", "class"])
-        writer.writeheader()
-        for segment in segments:
-            writer.writerow(
-                {
-                    "Audiofile": audio_filename,
-                    "start": segment.start,
-                    "end": segment.end,
-                    "class": segment.label.value,
-                }
-            )
+    try:
+        with open(output_path, "w", newline="") as f:
+            writer = csv.DictWriter(f, fieldnames=CSV_FIELDNAMES)
+            writer.writeheader()
+            for segment in segments:
+                writer.writerow(
+                    {
+                        "Audiofile": audio_filename,
+                        "start": segment.start,
+                        "end": segment.end,
+                        "class": segment.label.value,
+                    }
+                )
+    except OSError as err:
+        log.critical("Couldn't write CSV file.", exc_info=err)
+        return False
 
     log.info("CSV written → %s  (%d rows)", output_path, len(segments))
+    return True
